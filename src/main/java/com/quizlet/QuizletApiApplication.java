0b39c6773681731f36796a2e6cc2afcc1bbe8dd8
Kelvin Vu <kelvn.developer@gmail.com>
2024-02-20 12:46:48 +0700

    Init project

---
+++ src/main/java/com/quizlet/QuizletApiApplication.java
@@ -0,0 +1,13 @@
+package com.quizlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizletApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuizletApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/quizlet/QuizletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizletApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
