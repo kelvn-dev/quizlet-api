6b39f89edf1d86fa25388dcd65c5560195de6e82
Kelvin Vu <kelvn.developer@gmail.com>
2024-02-28 16:48:26 +0700

    forbid to increase score on private topic

--- src/main/java/com/quizlet/service/UserScoreService.java
+++ src/main/java/com/quizlet/service/UserScoreService.java
@@ -1,10 +1,13 @@
 package com.quizlet.service;
 
 import com.quizlet.dto.request.UserScoreReqDto;
+import com.quizlet.exception.ForbiddenException;
 import com.quizlet.mapping.UserScoreMapper;
+import com.quizlet.model.Topic;
 import com.quizlet.model.User;
 import com.quizlet.model.UserScore;
 import com.quizlet.repository.UserScoreRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -17,11 +20,19 @@ public class UserScoreService {
   private final UserScoreMapper userScoreMapper;
   private final UserScoreRepository userScoreRepository;
   private final UserService userService;
+  private final TopicService topicService;
   private final RabbitTemplate rabbitTemplate;
 
+  private void checkPublicTopic(UUID topicId) {
+    Topic topic = topicService.getById(topicId, false);
+    if (!topic.isPublic()) {
+      throw new ForbiddenException("Access denied");
+    }
+  }
+
   public UserScore create(JwtAuthenticationToken token, UserScoreReqDto dto) {
-    String auth0UserId = token.getToken().getSubject();
-    User user = userService.getByAuth0UserId(auth0UserId, false);
+    this.checkPublicTopic(dto.getTopicId());
+    User user = userService.getByToken(token, false);
     UserScore userScore = userScoreMapper.dto2Model(dto);
     userScore.setUserId(user.getId());
     userScore = userScoreRepository.save(userScore);
