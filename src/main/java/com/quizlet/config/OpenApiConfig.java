8df2f3c9c7f8658391de2725dcf7c92b168bf70a
Vũ Gia Khánh <khanhvg@vinbrain.net>
2024-10-30 10:41:23 +0700

    Update OpenApiConfig

--- src/main/java/com/quizlet/config/OpenApiConfig.java
+++ src/main/java/com/quizlet/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 // Swagger-ui endpoint: http://localhost:8080/swagger-ui/index.html#
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Configuration;
         @Info(
             title = "Quizlet API",
             version = "v1.0",
-            contact = @Contact(name = "Kelvin Vu", url = "https://github.com/kelvn-dev")))
+            contact = @Contact(name = "Kelvin Vu", url = "https://github.com/kelvn-dev")),
+    servers = @Server(url = "/"))
 @SecurityScheme(
     name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
